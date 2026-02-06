7b310592716169b84a7ae29044cbcfc715b65abe
adilrakik <adilrakik2001@gmail.com>
2026-02-06 14:13:26 +0100

    fix netlify blank page

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // ⬅️ هادي هي اللي ناقصة
   server: {
     host: "::",
     port: 8080,
